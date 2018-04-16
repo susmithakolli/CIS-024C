1d945811e97e42294ad3d31396b2516125b78681
Susmitha Kolli <32111936+susmithakolli@users.noreply.github.com>
2018-04-15 23:45:00 -0700

    Add files via upload

---
+++ helperfunction.py
@@ -0,0 +1,19 @@
+
+def add(num1,num2):
+    Sum = num1 + num2
+    return Sum
+
+def diff(num1,num2):
+    diff = num1 - num2
+    return diff
+
+def prod(num1,num2):
+    prod = num1*num2
+    return prod
+
+def great(num1,num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+    
